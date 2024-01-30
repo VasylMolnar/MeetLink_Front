239724e5da35855be4ada3e6a9e90b7bd51fc20b
VasylMolnar <vasylmolnar281@gmail.com>
2024-01-30 15:28:30 +0200

    add new feature

--- src/components/ChatCard/ChatCard.tsx
+++ src/components/ChatCard/ChatCard.tsx
@@ -1,5 +1,27 @@
-const ChatCard = () => {
-  return <div>ChatCard</div>;
+import "./ChatCard.scss";
+import { Link } from "react-router-dom";
+import logo from "../../assets/meetings1.jpg";
+
+type ChatCardProps = {
+  item: any;
+};
+
+const ChatCard = ({ item }: ChatCardProps) => {
+  return (
+    <div className="chatCard">
+      <Link to={item.id}>
+        <div className="header">
+          <img src={logo} alt="" className="logo" />
+          <div className="info">
+            <span className="user-name">{item.name}</span>
+            <span className="last-message">{item.lastMessage}</span>
+          </div>
+        </div>
+      </Link>
+
+      <button className="trash-btn">Видалити</button>
+    </div>
+  );
 };
 
 export default ChatCard;
--- src/pages/Chats/Chats.tsx
+++ src/pages/Chats/Chats.tsx
@@ -1,7 +1,90 @@
-import "./Chat.scss";
+import "./Chats.scss";
+import { Container } from "react-bootstrap";
+import ChatCard from "../../components/ChatCard/ChatCard";
 
 const Chats = () => {
-  return <main className="meet-link-chats"></main>;
+  {
+    /*fetch all chats*/
+  }
+  const data = [
+    {
+      id: "1",
+      name: "Sniff",
+      logo: "",
+      lastMessage: "ok",
+    },
+    {
+      id: "2",
+      name: "Фізика",
+      logo: "",
+      lastMessage: "by",
+    },
+    {
+      id: "3",
+      name: "Математика",
+      logo: "",
+      lastMessage: "ok see you later",
+    },
+    {
+      id: "4",
+      name: "meet name",
+      logo: "",
+      lastMessage: "ok see you later",
+    },
+    {
+      id: "5",
+      name: "meet name",
+      logo: "",
+      lastMessage: "ok see you later",
+    },
+    {
+      id: "6",
+      name: "meet name",
+      logo: "",
+      lastMessage: "ok see you later",
+    },
+    {
+      id: "7",
+      name: "meet name",
+      logo: "",
+      lastMessage: "ok see you later",
+    },
+    {
+      id: "8",
+      name: "meet name",
+      logo: "",
+      lastMessage: "ok see you later",
+    },
+    {
+      id: "9",
+      name: "meet name",
+      logo: "",
+      lastMessage: "Hello",
+    },
+    {
+      id: "10",
+      name: "meet name",
+      logo: "",
+      lastMessage: "ok see you later",
+    },
+  ];
+
+  return (
+    <main className="meet-link-chats">
+      <Container>
+        <input
+          className="message-finder"
+          type="text"
+          placeholder="Введіть ім'я або прізвище:"
+        />
+        <div className="chats">
+          {data.map((item) => (
+            <ChatCard item={item} key={item.id} />
+          ))}
+        </div>
+      </Container>
+    </main>
+  );
 };
 
 export default Chats;
