79d93ba07807c69eebbfcf475ab403dddb75638d
VasylMolnar <vasylmolnar281@gmail.com>
2024-05-02 10:22:31 +0300

    fix bugs

--- src/pages/InfoUser/InfoUser.tsx
+++ src/pages/InfoUser/InfoUser.tsx
@@ -4,7 +4,7 @@ import { useGetUserInfoQuery } from "../../features/user/userApiSlice";
 import { IErrorResponse, IUser } from "../../types/authTypes";
 import { Button, Container } from "react-bootstrap";
 import defaultIMG from "../../assets/defaultIMG.png";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { Loading } from "notiflix";
 import { Report } from "notiflix/build/notiflix-report-aio";
@@ -115,7 +115,6 @@ const InfoUser = () => {
                   }
                   sx={{ width: 200, height: 200 }}
                 />
-
                 <p
                   style={{
                     marginTop: "30px",
@@ -124,7 +123,65 @@ const InfoUser = () => {
                 >
                   {userInfo.username + " " + userInfo.surname}
                 </p>
+
+                {myId !== id &&
+                  userInfo.friendsList?.includes(
+                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                    //@ts-ignore
+                    myId
+                  ) && (
+                    <div>
+                      <Link to={`/calls/${userInfo._id}`}>
+                        <Button
+                          variant="outline-primary"
+                          style={{
+                            marginRight: "10px",
+                            maxWidth: "70px",
+                            width: "70px",
+                          }}
+                        >
+                          <svg
+                            stroke="currentColor"
+                            fill="currentColor"
+                            strokeWidth="0"
+                            viewBox="0 0 24 24"
+                            height="1em"
+                            width="1em"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path d="M16.57 22a2 2 0 0 0 1.43-.59l2.71-2.71a1 1 0 0 0 0-1.41l-4-4a1 1 0 0 0-1.41 0l-1.6 1.59a7.55 7.55 0 0 1-3-1.59 7.62 7.62 0 0 1-1.59-3l1.59-1.6a1 1 0 0 0 0-1.41l-4-4a1 1 0 0 0-1.41 0L2.59 6A2 2 0 0 0 2 7.43 15.28 15.28 0 0 0 6.3 17.7 15.28 15.28 0 0 0 16.57 22zM6 5.41 8.59 8 7.3 9.29a1 1 0 0 0-.3.91 10.12 10.12 0 0 0 2.3 4.5 10.08 10.08 0 0 0 4.5 2.3 1 1 0 0 0 .91-.27L16 15.41 18.59 18l-2 2a13.28 13.28 0 0 1-8.87-3.71A13.28 13.28 0 0 1 4 7.41zM20 11h2a8.81 8.81 0 0 0-9-9v2a6.77 6.77 0 0 1 7 7z"></path>
+                            <path d="M13 8c2.1 0 3 .9 3 3h2c0-3.22-1.78-5-5-5z"></path>
+                          </svg>
+                        </Button>
+                      </Link>
+
+                      <Link to={`/chats/${userInfo._id}`}>
+                        <Button
+                          variant="outline-success"
+                          style={{
+                            marginRight: "10px",
+                            maxWidth: "70px",
+                            width: "70px",
+                          }}
+                        >
+                          <svg
+                            stroke="currentColor"
+                            fill="currentColor"
+                            strokeWidth="0"
+                            viewBox="0 0 24 24"
+                            height="1em"
+                            width="1em"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path d="M16 2H8C4.691 2 2 4.691 2 8v13a1 1 0 0 0 1 1h13c3.309 0 6-2.691 6-6V8c0-3.309-2.691-6-6-6zm4 14c0 2.206-1.794 4-4 4H4V8c0-2.206 1.794-4 4-4h8c2.206 0 4 1.794 4 4v8z"></path>
+                            <path d="M7 9h10v2H7zm0 4h7v2H7z"></path>
+                          </svg>
+                        </Button>
+                      </Link>
+                    </div>
+                  )}
               </div>
+
               <div className="userEdit">
                 <div className="form">
                   <div
--- src/pages/MyFriends/MyFriends.tsx
+++ src/pages/MyFriends/MyFriends.tsx
@@ -198,12 +198,55 @@ const MyFriends = () => {
                       <span>{user.username + " " + user.surname}</span>
                     </div>
                   </Link>
-                  <Button
-                    variant="outline-danger"
-                    onClick={() => handleDeleteFollow(user._id)}
-                  >
-                    Не стежити
-                  </Button>
+
+                  <div>
+                    <Link to={`/calls/${user._id}`}>
+                      <Button
+                        variant="outline-primary"
+                        style={{ marginRight: "10px" }}
+                      >
+                        <svg
+                          stroke="currentColor"
+                          fill="currentColor"
+                          strokeWidth="0"
+                          viewBox="0 0 24 24"
+                          height="1em"
+                          width="1em"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M16.57 22a2 2 0 0 0 1.43-.59l2.71-2.71a1 1 0 0 0 0-1.41l-4-4a1 1 0 0 0-1.41 0l-1.6 1.59a7.55 7.55 0 0 1-3-1.59 7.62 7.62 0 0 1-1.59-3l1.59-1.6a1 1 0 0 0 0-1.41l-4-4a1 1 0 0 0-1.41 0L2.59 6A2 2 0 0 0 2 7.43 15.28 15.28 0 0 0 6.3 17.7 15.28 15.28 0 0 0 16.57 22zM6 5.41 8.59 8 7.3 9.29a1 1 0 0 0-.3.91 10.12 10.12 0 0 0 2.3 4.5 10.08 10.08 0 0 0 4.5 2.3 1 1 0 0 0 .91-.27L16 15.41 18.59 18l-2 2a13.28 13.28 0 0 1-8.87-3.71A13.28 13.28 0 0 1 4 7.41zM20 11h2a8.81 8.81 0 0 0-9-9v2a6.77 6.77 0 0 1 7 7z"></path>
+                          <path d="M13 8c2.1 0 3 .9 3 3h2c0-3.22-1.78-5-5-5z"></path>
+                        </svg>
+                      </Button>
+                    </Link>
+
+                    <Link to={`/chats/${user._id}`}>
+                      <Button
+                        variant="outline-success"
+                        style={{ marginRight: "10px" }}
+                      >
+                        <svg
+                          stroke="currentColor"
+                          fill="currentColor"
+                          strokeWidth="0"
+                          viewBox="0 0 24 24"
+                          height="1em"
+                          width="1em"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M16 2H8C4.691 2 2 4.691 2 8v13a1 1 0 0 0 1 1h13c3.309 0 6-2.691 6-6V8c0-3.309-2.691-6-6-6zm4 14c0 2.206-1.794 4-4 4H4V8c0-2.206 1.794-4 4-4h8c2.206 0 4 1.794 4 4v8z"></path>
+                          <path d="M7 9h10v2H7zm0 4h7v2H7z"></path>
+                        </svg>
+                      </Button>
+                    </Link>
+
+                    <Button
+                      variant="outline-danger"
+                      onClick={() => handleDeleteFollow(user._id)}
+                    >
+                      Не стежити
+                    </Button>
+                  </div>
                 </div>
               );
             })}
